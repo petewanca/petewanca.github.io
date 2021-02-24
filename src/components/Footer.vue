bcfb6f8d0e5376a0d5d39dd07ab33cfd97d3030e
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-02-23 21:39:46 -0600

    adds bootstrap 5 and bootstrap icons

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,24 +2,35 @@
   <div>
     <div>
       <a href="https://www.facebook.com/petewancadrums/" target="_blank"
-        >Facebook</a
-      >
+        ><BIconFacebook
+      /></a>
       <a href="https://www.instagram.com/petewanca" target="_blank"
-        >Instagram</a
-      >
+        ><BIconInstagram
+      /></a>
       <a
         href="https://www.youtube.com/channel/UCCnzOSrTXbuYnooC73mmrLA"
         target="_blank"
-        >YouTube</a
-      >
+        ><BIconYoutube
+      /></a>
     </div>
     <div>Copyright © 2021 Pete Wanca - All Rights Reserved</div>
   </div>
 </template>
 
 <script>
+import {
+  BIconYoutube,
+  BIconFacebook,
+  BIconInstagram,
+} from "bootstrap-icons-vue";
+
 export default {
   name: "Footer",
+  components: {
+    BIconYoutube,
+    BIconFacebook,
+    BIconInstagram,
+  },
 };
 </script>
 
