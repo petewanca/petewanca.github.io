f0d5ef9bf84cd66dbe35f01bb21e764859c2c5a8
Terrence Mahnken <terrence.mahnken@hcahealthcare.com>
2021-03-16 21:33:34 -0500

    add links to ids; crops logo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const mobileNavbar = document.getElementById("mobile-navbar");
 const closeMobileMenu = document.getElementById("close-mobile-menu");
+const mobileNavItems = document.querySelectorAll(".mobile-nav-item");
 
 function openNav() {
     mobileNavbar.style.width = "100%";
@@ -11,8 +12,15 @@ function closeNav() {
 }
 
 function closeOnLink() {
+    mobileNavbar.style.width = "0%";
     mobileNavbar.style.transition = "none";
 }
+
+mobileNavItems.forEach((element) => {
+    element.addEventListener("click", function () {
+        closeNav();
+    });
+});
 
 hamburgerMenu.addEventListener("click", function () {
     openNav();
