30d1f8802c8eadbf0bfbe81a9b8081dc309651de
Terrence Mahnken <terrencemm2@gmail.com>
2021-08-16 21:06:18 -0500

    feat: adds toggle to audio player and gif

---
+++ assets/audio/metadata.js
@@ -0,0 +1,12 @@
+const audioMetadata = [
+    {
+        title: "Running Away",
+        artist: "Bucky Shorts",
+        filename: "running-away-master.mp3",
+    },
+    {
+        title: "Sunny Pry",
+        artist: "Body Origami",
+        filename: "sunny-pry-master.mp3",
+    },
+];
