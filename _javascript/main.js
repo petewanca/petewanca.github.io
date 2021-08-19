b7a8db3714dd65f11412620d63ec8b4d60ac9a98
Terrence Mahnken <terrencemm2@gmail.com>
2021-08-18 20:51:51 -0500

    feat: dyanmically change track number and direction arrow

--- _javascript/main.js
+++ _javascript/main.js
@@ -1,3 +1,4 @@
+// GLOBAL VARIABLES
 const mainEl = document.getElementsByTagName("main");
 const enterEl = document.getElementById("enter");
 const homeEl = document.getElementById("home");
@@ -6,6 +7,7 @@ const songTitleEl = document.getElementById("song-title");
 const songArtistEl = document.getElementById("song-artist");
 const numerator = document.getElementById("numerator");
 const denominator = document.getElementById("denominator");
+const trackArrowEl = document.getElementById("track-arrow");
 
 homeEl.style.display = "none";
 navEl.style.display = "none";
@@ -13,6 +15,20 @@ navEl.style.display = "none";
 const enterBtn = document.getElementById("enter-btn");
 
 const soundWaveGraphic = document.getElementById("sound-wave");
+
+// FUNCTIONS
+handleNumeratorDisplay = () => {
+    numerator.textContent = currentTrackNumber + 1;
+};
+
+// FUNCTION CALLS
+// Enter Site Button
+enterBtn.addEventListener("click", () => {
+    enterEl.style.display = "none";
+    navEl.style.display = "flex";
+    homeEl.style.display = "flex";
+    audioPlayer.play();
+});
 
 // Audio Player
 let currentTrackNumber = 0;
@@ -45,10 +61,26 @@ soundWaveGraphic.addEventListener("click", (event) => {
     }
 });
 
-// Enter Site Button
-enterBtn.addEventListener("click", () => {
-    enterEl.style.display = "none";
-    navEl.style.display = "flex";
-    homeEl.style.display = "flex";
-    audioPlayer.play();
+// Change Track Control
+trackArrowEl.addEventListener("click", (event) => {
+    // handleNumeratorDisplay(event.target.dataset);
+    const { direction } = event.target.dataset;
+
+    if (direction === "forward") {
+        currentTrackNumber++;
+        if (currentTrackNumber === audioMetadata.length - 1) {
+            trackArrowEl.classList.remove("fa-chevron-circle-right");
+            trackArrowEl.classList.add("fa-chevron-circle-left");
+            trackArrowEl.setAttribute("data-direction", "backward");
+        }
+    } else if (direction === "backward") {
+        currentTrackNumber--;
+        if (currentTrackNumber === 0) {
+            trackArrowEl.classList.remove("fa-chevron-circle-left");
+            trackArrowEl.classList.add("fa-chevron-circle-right");
+            trackArrowEl.setAttribute("data-direction", "forward");
+        }
+    }
+
+    handleNumeratorDisplay();
 });
