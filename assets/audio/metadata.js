4771c4961d6b9d8769f28de38f5d171005381fa7
Terrence Mahnken <terrencemm2@gmail.com>
2021-08-22 18:54:07 -0500

    fix: autoplay track based on global isPlaying value

--- assets/audio/metadata.js
+++ assets/audio/metadata.js
@@ -9,4 +9,14 @@ const audioMetadata = [
         artist: "Body Origami",
         filename: "sunny-pry-master.mp3",
     },
+    {
+        title: "Running Away",
+        artist: "Bucky Shorts",
+        filename: "running-away-master.mp3",
+    },
+    {
+        title: "Sunny Pry",
+        artist: "Body Origami",
+        filename: "sunny-pry-master.mp3",
+    },
 ];
