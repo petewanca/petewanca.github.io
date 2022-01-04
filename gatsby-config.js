e2c6905a2ec23a0c6c946b87655cf50ae4fbd3ec
Terrence Mahnken <terrencemm2@gmail.com>
2022-01-03 22:23:46 -0600

    chore: wire up sass and bulma

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: "https://www.petewanca.com",
+        title: "Pete Wanca",
+    },
+    plugins: [
+        "gatsby-plugin-root-import",
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: "GatsbyJS",
+                short_name: "GatsbyJS",
+                start_url: "/",
+                background_color: "#6b37bf",
+                theme_color: "#6b37bf",
+                // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+                // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+                display: "browser",
+                icon: "src/images/icon.png", // This path is relative to the root of the site.
+                // An optional attribute which provides support for CORS check.
+                // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+                // Any invalid keyword or empty string defaults to `anonymous`
+                crossOrigin: `use-credentials`,
+            },
+        },
+        {
+            resolve: "gatsby-plugin-sass",
+            options: {
+                useResolveUrlLoader: true,
+            },
+        },
+    ],
+};
