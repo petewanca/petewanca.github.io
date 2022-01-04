46d045a1e9da42522515f9bfa5afa13399efe873
Terrence Mahnken <terrencemm2@gmail.com>
2022-01-03 22:26:05 -0600

    fix: strip boilerplate and move enter page code

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "styles/index.scss";
+
+const IndexPage = () => (
+    <main id="enter">
+        <div id="enter-div1">
+            <h1>Pete Wanca</h1>
+            <h2>
+                Mixing & Mastering
+                <br />
+                Engineer
+            </h2>
+        </div>
+        <div id="enter-div2">
+            <i className="fas fa-headphones-alt"></i>
+            To enhance your experience,
+            <br />
+            please turn on your sound.
+        </div>
+        <div id="enter-btn">ENTER</div>
+    </main>
+);
+
+export default IndexPage;
