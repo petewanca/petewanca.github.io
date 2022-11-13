d657b8ff112b7d935a8ff9cd15739a1a9621c637
Terrence Mahnken <terrencemm2@gmail.com>
2022-11-13 13:40:02 -0600

    feat: add audio player controls comp

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ import { useAudioPlayer } from "context/audioPlayerContext";
 // Custom Components
 import NavBar from "components/NavBar";
 import AudioPlayerDisplay from "components/AudioPlayerDisplay";
+import AudioPlayerControls from "components/AudioPlayerControls";
 
 // Style
 import "styles/home.scss";
@@ -21,6 +22,12 @@ const HomePage = () => {
           tracks={state.tracks}
           currentTrackNo={state.currentTrackNo}
           currentTrack={state.currentTrack}
+          dispatch={dispatch}
+        />
+        <AudioPlayerControls
+          isPlaying={state.isPlaying}
+          tracks={state.tracks}
+          currentTrackNo={state.currentTrackNo}
           dispatch={dispatch}
         />
       </main>
