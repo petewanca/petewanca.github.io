80c7928807d8430d82a4b2ab5dbdda478829a565
Terrence Mahnken <terrencemm2@gmail.com>
2022-11-13 12:40:42 -0600

    refactor: add prop-types; change to display only comp

--- src/components/AudioPlayerDisplay/index.jsx
+++ src/components/AudioPlayerDisplay/index.jsx
@@ -15,35 +15,38 @@ const AudioPlayerDisplay = ({
   currentTrackNo,
   currentTrack,
   dispatch,
-}) => {
-  return (
-    <div className={`${audioPlayerStyle.audioPlayerContainer}`}>
-      <div className={`${audioPlayerStyle.trackDetailsContainer}`}>
-        <div className="title is-1">{currentTrack.title}</div>
-        <div className="subtitle is-3">by {currentTrack.artist}</div>
-      </div>
-      <div>
-        {currentTrackNo !== 0 && (
-          <FontAwesomeIcon
-            onClick={() => dispatch({ type: "AUDIO_PLAYER_BACKWARD" })}
-            className={`${audioPlayerStyle.playerControl}`}
-            size="6x"
-            icon={faArrowAltCircleLeft}
-          />
-        )}
-        {currentTrackNo !== tracks.length - 1 && (
-          <FontAwesomeIcon
-            onClick={() => dispatch({ type: "AUDIO_PLAYER_FORWARD" })}
-            className={`${audioPlayerStyle.playerControl} pl-3`}
-            size="6x"
-            icon={faArrowAltCircleRight}
-          />
-        )}
-      </div>
+}) => (
+  <div className={`${audioPlayerStyle.audioPlayerContainer}`}>
+    <div className={`${audioPlayerStyle.trackDetailsContainer}`}>
+      <div className="title is-1">{currentTrack.title}</div>
+      <div className="subtitle is-3">by {currentTrack.artist}</div>
     </div>
-  );
-};
+    <div>
+      {currentTrackNo !== 0 && (
+        <FontAwesomeIcon
+          onClick={() => dispatch({ type: "AUDIO_PLAYER_BACKWARD" })}
+          className={`${audioPlayerStyle.playerControl}`}
+          size="6x"
+          icon={faArrowAltCircleLeft}
+        />
+      )}
+      {currentTrackNo !== tracks.length - 1 && (
+        <FontAwesomeIcon
+          onClick={() => dispatch({ type: "AUDIO_PLAYER_FORWARD" })}
+          className={`${audioPlayerStyle.playerControl} pl-3`}
+          size="6x"
+          icon={faArrowAltCircleRight}
+        />
+      )}
+    </div>
+  </div>
+);
 
-AudioPlayerDisplay.propTypes = {};
+AudioPlayerDisplay.propTypes = {
+  tracks: PropTypes.array,
+  currentTrackNo: PropTypes.number,
+  currentTrack: PropTypes.object,
+  dispatch: PropTypes.func,
+};
 
 export default AudioPlayerDisplay;
